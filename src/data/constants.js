8bb55206e559274534c63958a507962cccf02e96
AmaniFad <amani.fad12@gmail.com>
2024-12-20 14:11:55 +0000

    porfolio2.0

---
+++ src/data/constants.js
@@ -0,0 +1,289 @@
+import project1 from "../utils/projects/p1.png";
+import project2 from "../utils/projects/p2.png";
+import project3 from "../utils/projects/p3.jpg";
+import project4 from "../utils/projects/p4.png";
+import project5 from "../utils/projects/p5.png";
+import project6 from "../utils/projects/p6.jpg";
+import project7 from "../utils/projects/p7.png";
+
+export const Bio = {
+  name: "Amani Fadli Dokkali",
+  roles: [
+    "Game Developer",
+    "Programmer",
+  ],
+  description:
+    "Graduated of Higher Degree in Multimedia Applications and Video Games Development. I know Web Programming, Database, Applications Mobil and Video games.I am interested in joining a company as a junior programmer to put my skills into practice and provide the company's users with the best digital experience. As a student, I have created several games, programs, and websites, which I have attached. I always work with the desire to improve and adapt to the company and environment that I find myself working at.",
+  github: "https://github.com/AmaniFadli",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/amani-fadli-dokkali-81b8172a0/",
+  itchio: "https://amanifd.itch.io/",
+};
+
+export const skills = [
+  {
+    title: "Game Development",
+    skills: [
+      {
+        name: "Unity",
+        image: "https://yt3.googleusercontent.com/4YAaX7CsQ-paadHsNvC6wdv2nE_7VTKpoNJOTwFhHL4rQqLC8WFe6mNxL-z-e6sfHZbHG-ucPA=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        name: "C#",
+        image: "https://www.jetbrains.com/guide/assets/csharp-logo-265a149e.svg",
+      },
+      {
+        name: "Unreal",
+        image:
+          "https://bairesdev.mo.cloudinary.net/blog/2022/08/ue-logo-1400x788-1400x788-8f185e1e3635-1.jpg?tx=w_1920,q_auto",
+      },
+      
+      {
+        name: "C++",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Web Development",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Node Js",
+        image:
+          "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "3D Max",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi_D8rh1f1pabHyt_OEo-mXInBIicX5EKwuw&s",
+      },
+      {
+        name: "Blender",
+        image:
+          "https://i.pcmag.com/imagery/reviews/00CHLsbAPPKloTAg2PZlVBC-1..v1662670755.jpg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Simulation Programmer Intern, Drone Simulator",
+    company: "AEROFOR",
+    date: "Feb 2023 - Present",
+    desc: "Interactive Simulator Development: Created an immersive simulator with realistic physics and detailed environments, offering intuitive controls, training modules, and real-time feedback. Level Design: Designed varied and progressively challenging levels with interactive elements, optimized for efficient performance and an engaging experience. User Management Development: Implemented personalised and secure profiles, with performance analysis tools, customization options, and social features for an enhanced experience.",
+    skills: [
+     "Unity", 
+     "C#", 
+     "PlayFab"
+    ],
+  },
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C4D0BAQGkZ66n1TziFw/company-logo_200_200/company-logo_200_200/0/1630559246814/institut_ausis_march_logo?e=2147483647&v=beta&t=ZmocxYNTLmdW5JOFY8aRYIjcQmTR3sejRkYJxAaVL7o",
+    school: "Institut Pedralbes,Barcelona, Spain",
+    date: "Sept 22- Jun 24 ",
+    grade: "8,02",
+    degree: "HNC in Development of Muliplatform Apps & Videogames",
+  },
+  {
+    id: 1,
+    img: "https://pbs.twimg.com/media/GXDQfkgX0AEmCTM.jpg",
+    school:"Institut Isaac Albeniz, Badalona, Spain ",
+    date: "Sept 20 – May 22 ",
+    grade: "5,69",
+    desc: "Research Project: “ Jupiter: An Impossible Star “, based on the study of the formation, composition and impact of Jupiter on the Solar System and the potential of a binary system by programming a simulation. ",
+    degree: "Spanish Baccalaureate in Technology,",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "DAL(IA)",
+    description:
+      "My classmates, our tutor, and I participated in 'Premios Transferencia de conocimiento de la FP'. This competition addresses societal issues aligned with the Sustainable Development Goals (SDGs), focusing on Goal 5 (gender equality) and also relating to Goals 3, 4, 9, and 10. We identified the growing issue of digital gender-based violence, which needs innovative solutions. Detecting these patterns in conversations can be challenging. Our solution is DALIA, a mobile app using AI to recognize verbal violence through patterns, words, and phrases in conversations. It also includes behavior analysis tests to help users become more aware of their communication.",
+    image: project1,
+    tags: [
+      "Java", 
+      "VisualStudio", 
+      "JSON"
+    ],
+    category: "prototype",
+    webapp: "https://www.youtube.com/watch?v=N1rjROAsX2Y&t=1s",
+    member: [
+      {
+        name: "Alma Colina",
+        img: "https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg",
+        linkedin: "https://www.linkedin.com/in/almacolina/",
+      },
+      {
+        name: "Emma Casadevall Hernández",
+        img: "https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg",
+        linkedin: "https://www.linkedin.com/in/emma-casadevall-hern%C3%A1ndez-362a85226/",
+      },
+      {
+        name: "Laura Carrillo Espigares",
+        img: "https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg",
+        linkedin: "https://www.linkedin.com/in/lcarrilloe/",
+      },
+      {
+        name: "Clàudia Martín",
+        img: "https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg",
+        linkedin: "https://www.linkedin.com/in/cl%C3%A0udia-mart%C3%ADn-993933226/",
+      },
+
+    ],
+  },
+  {
+    id: 0,
+    title: "HOLLYWOOD NIGHTMARE",
+    date: "Des 2023 - May 2024",
+    description:
+      "Welcome to Hollywood Nightmare! Join a mother and daughter on an epic adventure in a Barcelona movie studio where film characters come to life. Battle across three sets: Barbie, Star Wars, and The Matrix. With a unique visual style blending retro and avant-garde, Hollywood Nightmare offers action, fun, and a feminist twist. Available on Xbox, PlayStation, PC/MAC, and mobile devices.",
+    image: project2,
+    tags: ["Unity", "C#", "Blender", "FMod"],
+    category: "games",
+    webapp: "https://amanifd.itch.io/hollywood-nightmare",
+    /*member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],*/
+  },
+  {
+    id: 1,
+    title: "FINAL MISSION",
+
+    description:
+      "It's a first-person 3D Escape Room game, where players must solve a series of puzzles and riddles in order to 'escape' from a themed room or environment within a limited time.",
+    image:project3,
+    tags: ["Unity", "C#"],
+    category: "games",
+    webapp: "https://amanifd.itch.io/final-mission",
+  },
+  {
+    id: 2,
+    title: "UNFORESEEN",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Dive into an unforgettable adventure with our game, a perfect blend of 3D enviroment and charming 2D pixel art. Navigate a dark dungeon, relying on your skills and powers to uncover the hidden treasure. This game had its genesis during one of the JAM sponsored by FX BARCELONA FILM SCHOOL. The JAM focused on creating a game in just two days, following the theme of 'Into the unknown'. We are very happy and satisfied, as we achieved first place for the best game in the Jam. It was an enjoyable and ambitious experience thanks to FX Barcelona Film School.",
+    image: project4,
+    tags: ["Unity", "C#", "Blender", "Photoshop"],
+    category: "games",
+    webapp: "https://amanifd.itch.io/unforeseen",
+    /*member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],*/
+  },
+  {
+    id: 3,
+    title: "THE GRAVEKEEPER",
+    description:
+      "Experience 'The Gravekeeper' a horror game where cleaning tombs turns into a survival challenge. As a cemetery worker, you'll face unique ghosts with specific rules. Clean in the right order to earn their trust and uncover their stories—or risk their wrath. Can you navigate the darkness and escape their vengeance?",
+    image:project7,
+    tags: ["Unity", "C#", "Blender", "Photoshop"],
+    category: "games",
+    webapp: "https://sttrangge.itch.io/the-gravekeeper",
+  },
+  {
+    id: 10,
+    title: "ESCAPE TO THE DEPTH",
+    description:
+      "In 'Escape the Depth' the player is trapped in a deep underground well and must overcome challenging parkour courses to escape. Developed in 3D using the Unity engine, this game tests your agility and precision. With each jump, climb, and sprint, the risk is high: one mistake could lead you to fall into the abyss. This game had its genesis during one of the JAM sponsored by FRAME GAMES. The JAM focused on creating a game in just two days, following the theme of 'deep'. I am very proud to have participated with my teammates and for the pleasant experience provided by Frame Games, even though we didn't win anything",
+    image:project5,
+    tags: ["Unity", "C#", "Blender", "Photoshop"],
+    category: "games",
+    webapp: "https://sttrangge.itch.io/escape-the-depth",
+  },
+  {
+    id: 4,
+    title: "COZY CUP",
+    description:
+      "It's a 3D game made with the Unity engine. This game had its genesis during one of the JAMS sponsored by Itch.io. The JAM in question was conducted during Winter MelonJam 2023 and focused on creating a game in just two days, following the theme of charm. In this challenge, I collaborated with three colleagues, and our participation was framed in the category of beginners/students.",
+    image:project6,
+    tags: ["Unity", "C#", "Blender", "Photoshop"],
+    category: "games",
+    webapp: "https://sttrangge.itch.io/cozy-cup",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
